2f6636127d0405b782ee6dc276b3512e610cf49b
bethebikash <bhattaraibikash55@gmail.com>
2019-12-09 21:48:41 +0545

    Recycler View is done by listing mails.

--- app/src/main/java/com/bhattaraibikash/recyclerviewassignment/MainActivity.java
+++ app/src/main/java/com/bhattaraibikash/recyclerviewassignment/MainActivity.java
@@ -1,14 +1,33 @@
 package com.bhattaraibikash.recyclerviewassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvMailList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvMailList = findViewById(R.id.rvMailList);
+
+        List<MailList> mailLists = new ArrayList<>();
+        mailLists.add(new MailList("Hello dude!", "Hi, I am Someone writinh this message.", "04:21", R.drawable.personone));
+        mailLists.add(new MailList("My Assignment", "I have uploaded my assignment in the GitHub.", "06:10", R.drawable.persontwo));
+        mailLists.add(new MailList("The Problem", "Hello Sir, I am facing problem in recycler view.", "02:43", R.drawable.personthree));
+        mailLists.add(new MailList("Sample title", "And this is the sample message", "04:21", R.drawable.personfour));
+
+        MailListAdapter mailListAdapter = new MailListAdapter(this, mailLists);
+        rvMailList.setAdapter(mailListAdapter);
+        rvMailList.setLayoutManager(new LinearLayoutManager(this));
     }
 }
